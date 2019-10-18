d0f165691fd85f7175a6356d06712a0e4cad651e
ricardoc333 <ricardo_c3@hotmail.com>
2019-10-18 01:18:47 -0300

    Mejoras parciales Windows
    Mejoras parciales Windows

--- src/main/java/cl/timestamp/devops/poc/SpringBootApplicationLuncher.java
+++ src/main/java/cl/timestamp/devops/poc/SpringBootApplicationLuncher.java
@@ -2,8 +2,11 @@ package cl.timestamp.devops.poc;
 
 import cl.timestamp.devops.poc.controller.HelloWorldController;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-public class SpringBootApplicationLuncher {
+@SpringBootApplication
+public class SpringBootApplicationLuncher extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldController.class, args);
     }
