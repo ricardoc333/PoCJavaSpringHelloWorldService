67030730e342e3f54746469e76ff56c8b5be0a74
ricardoc333 <55106056+ricardoc333@users.noreply.github.com>
2019-11-02 22:26:12 -0300

    22.26
    22.26

--- src/main/java/cl/timestamp/devops/poc/controller/HelloWorldController.java
+++ src/main/java/cl/timestamp/devops/poc/controller/HelloWorldController.java
@@ -11,6 +11,6 @@ public class HelloWorldController {
     @RequestMapping("/hello")
     @ResponseBody
     public String sayHello() {
-        return "Hello World PoC Java Ricardo Sábado 13:22.45 Timestamp!!!";
+        return "Hello World PoC Java Ricardo Sábado 22:26.00 Timestamp!!!";
     }
 }
